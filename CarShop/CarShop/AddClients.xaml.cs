11e637f7239da981c14e467d79ea9510e914b7ff
tereshkovychura@gmail.com <tereshkovychura@gmail.com>
2019-08-21 11:47:55 +0300

    work with AddClients

--- CarShop/CarShop/AddClients.xaml.cs
+++ CarShop/CarShop/AddClients.xaml.cs
@@ -14,7 +14,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CarShop.Entities;
+using CarShop.ViewModels;
 using Microsoft.Win32;
+using WPFAnimal.Extensions;
 
 namespace CarShop
 {
@@ -22,8 +25,9 @@ namespace CarShop
     /// Interaction logic for AddClients.xaml
     /// </summary>
     public partial class AddClients : Window
-    {      
-        public ObservableCollection<ClientVM> clients = new ObservableCollection<ClientVM>();
+    {
+        private ObservableCollection<ClientViewModels> ClientVM { get; set; }
+        private readonly EFcontext _context;
         public bool check = false;
         public string ImgName;
         string imageFolderSave = "ClientsPictures";
@@ -32,10 +36,27 @@ namespace CarShop
         {
             InitializeComponent();         
             PathImagDic = System.IO.Path.Combine(Directory.GetCurrentDirectory(), imageFolderSave);
+            ClientVM = new ObservableCollection<ClientViewModels>();
+            _context = new EFcontext();
+            FillDB();
+        }
+        void FillDB()
+        {
+            try
+            {
+                var query = _context.Makes.AsQueryable();
+                var list = query.Select(at => new ClientViewModels
+                {
+                    Id = at.Id,
+                    Name = at.Name
+                }).ToList();
+                ClientVM.Clear();
+                ClientVM.AddRange(list);
+            }
+            catch { }
         }
         private void BtnImage_Click(object sender, RoutedEventArgs e)
         {
-            string ImgName;
             string imageFolderSave = "ClientsPictures";
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) " +
@@ -64,7 +85,41 @@ namespace CarShop
         }
         private void UserAdd()
         {
+            if (txtName.Text == "" && txtSurname.Text == "" && txtPhone.Text == "" && ImgName=="")
+            {
+                MessageBox.Show("Please, fill all lines.");
+            }
+            else
+            {
+                var list = _context.Clients.AsQueryable().ToList();
+                bool c = false;
+                foreach (var item in list)
+                {
+                    if (item.Phone == txtPhone.Text)
+                        c = true;
+                }
+                if (c == true)
+                {
+                    MessageBox.Show("Error");
+                }
+                if (c == false)
+                {
+                    _context.Clients.Add(new Entities.Client {
+                        Name = txtName.Text + " " + txtSurname.Text,
+                        Image = ImgName,
+                        Phone = txtPhone.Text,
+                        Password = txtPassword.Text
+                    });
+                    _context.SaveChanges();
+                    FillDB();
+                    MessageBox.Show("You succesfully registered!");
+                }
+            }
+        }
 
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            UserAdd();
         }
     }
     }
