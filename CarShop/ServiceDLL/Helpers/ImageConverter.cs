7d91f83e9e39174dd0f135a46c2d47999d3aa34f
Kirito <Kirito@A24PC2>
2019-10-22 12:18:13 +0300

    work with image

--- CarShop/ServiceDLL/Helpers/ImageConverter.cs
+++ CarShop/ServiceDLL/Helpers/ImageConverter.cs
@@ -20,6 +20,17 @@ namespace ServiceDLL.Helpers
                 // Convert byte[] to Base64 String
                 string base64String = Convert.ToBase64String(imageBytes);
                 return base64String;
+            }          
+        }
+        public static Image Base64ToImage(this string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(ms, true);
+                return image;
             }
         }
     }
--- CarShop/ServiceDLL/Models/ClientModels.cs
+++ CarShop/ServiceDLL/Models/ClientModels.cs
@@ -1,30 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceDLL.Models
-{  
-        public class ClientVM
-        {
-            public int Id { get; set; }
+{
+    public class ClientVM
+    {
+        public int Id { get; set; }
 
-            public string Name { get; set; }
+        public string Name { get; set; }
 
-            public string Phone { get; set; }
+        public string Phone { get; set; }
 
-            public string Image { get; set; }
+        public string Image { get; set; }
+    }
+    public class ClientDataGridVM
+    {
+        public int Id { get; set; }
 
-        }
-        public class ClientAddVM
-        {
-            public string Name { get; set; }
-            public string Phone { get; set; }
-            public string Image { get; set; }
-        }
-        public class ClientDeleteVM
-        {
-            public int Id { get; set; }
-        }
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public Image Image { get; set; }
+
+    }
+    public class ClientAddVM
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Image { get; set; }
+    }
+    public class ClientDeleteVM
+    {
+        public int Id { get; set; }
+    }
 }
