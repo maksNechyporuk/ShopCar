63364172d92f9a82e40578e2623dc5770010b827
MaksumNechiporuk <maksum.nec@gmail.com>
2019-11-07 18:20:47 +0200

    good changed models

--- CarShop/CarShop/EmployeersWindows/ShowEmployees.xaml.cs
+++ CarShop/CarShop/EmployeersWindows/ShowEmployees.xaml.cs
@@ -32,7 +32,7 @@ namespace CarShop.EmployeersWindows
 
         async void FillDG()
         {
-            users = await servise.GetUserAsync();
+            users = await servise.GetUserAsync(null);
             dgShowEmployees.ItemsSource = users;
         }
 
