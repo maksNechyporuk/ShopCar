4bfe93628baaa26d49d5b7857c1004d48cd70df5
Kirito <Kirito@A24PC2>
2019-11-13 12:08:39 +0200

    large work with image

--- CarShop/ServiceDLL/Models/ClientModels.cs
+++ CarShop/ServiceDLL/Models/ClientModels.cs
@@ -14,6 +14,7 @@ namespace ServiceDLL.Models
         public string Name { get; set; }
 
         public string Phone { get; set; }
+        public string UniqueName { get; set; }
 
         public string Image { get; set; }
 
@@ -26,6 +27,7 @@ namespace ServiceDLL.Models
         public string Name { get; set; }
 
         public string Phone { get; set; }
+        public string UniqueName { get; set; }
 
         public string Email { get; set; }
 
@@ -35,6 +37,7 @@ namespace ServiceDLL.Models
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Image { get; set; }
+        public string UniqueName { get; set; }
         public string Email { get; set; }
     }
     public class ClientDeleteVM
