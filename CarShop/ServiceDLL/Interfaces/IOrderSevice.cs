47021feefeafc777a737a82b7af70e77e8e4da13
MaksumNechiporuk <maksum.nec@gmail.com>
2019-11-14 00:05:49 +0200

    good work

---
+++ CarShop/ServiceDLL/Interfaces/IOrderSevice.cs
@@ -0,0 +1,18 @@
+﻿using ServiceDLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDLL.Interfaces
+{
+    interface IOrderService
+    {
+
+        List<OrderShowVM> GetOrders();
+        Task<List<OrderShowVM>> GetOrdersAsync();
+        string Create(OrderAddVM name);
+        Task<string> CreateAsync(OrderAddVM filter);
+    }
+}
---
+++ CarShop/ServiceDLL/Models/OrderModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDLL.Models
+{
+    public class OrderVM
+    {
+        public ClientVM Client { get; set; }
+        public CarVM Car { get; set; }
+    }
+    public class OrderShowVM
+    {
+        public string Client { get; set; }
+        public string Car { get; set; }
+    }
+
+    public class OrderAddVM
+    {
+        public ClientVM Client { get; set; }
+        public CarVM Car { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+
+
+}
